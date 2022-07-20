affae06a3069f2fd53687a3bb9a62823ebb8fc7b
Albino Marques dos Santos <108479576+Albino-Marques@users.noreply.github.com>
2022-07-19 21:10:19 -0300

    Upload das alterações totais
    Upload de todas as alterações anteriormente feitas.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# interface-da-netflix-clone
- Um projeto com intuito de construir, praticar e aprimorar as habilidades em JavaScript, HTML5 e CSS3.
+# Projeto-RECRIAR-A-INTERFACE-DA-NETFLIX
+ Projeto da DIO para por em prática o que foi aprendido em HTML5, CSS3, JavaScript e Bootstrap até o momento.
+
+ Este projeto está em desenvolvimento, mas é um esboço do básico que consigo fazer. 
---
+++ js/main.js
@@ -0,0 +1,37 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('.owl-carousel').owlCarousel({
+    items:1,
+    merge:true,
+    loop:true,
+    margin:10,
+    video:true,
+    lazyLoad:true,
+    center:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
